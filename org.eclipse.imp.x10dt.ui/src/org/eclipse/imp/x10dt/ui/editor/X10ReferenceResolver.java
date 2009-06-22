12a94de774b4a92213ede325d2e6cbb289d55ee0
No Author <noone@domain>
2009-06-22 21:46:08 +0000

    This commit was manufactured by cvs2svn to create tag 'X10OpenSourceRelease-1_0'.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/tags/X10OpenSourceRelease-1_0@9728 a326200e-df2c-42e0-81fa-597e909af41d

--- org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10ReferenceResolver.java
+++ org.eclipse.imp.x10dt.ui/src/org/eclipse/imp/x10dt/ui/editor/X10ReferenceResolver.java
@@ -8,10 +8,8 @@ import org.eclipse.uide.parser.IParseController;
 import polyglot.ast.Ambiguous;
 import polyglot.ast.Call;
 import polyglot.ast.Field;
-import polyglot.ast.Id;
 import polyglot.ast.Local;
 import polyglot.ast.LocalDecl;
-import polyglot.ast.New;
 import polyglot.ast.Node;
 import polyglot.ast.Receiver;
 import polyglot.ast.Special;
@@ -22,30 +20,18 @@ import polyglot.types.LocalInstance;
 import polyglot.types.MethodInstance;
 import polyglot.types.ReferenceType;
 import polyglot.visit.NodeVisitor;
-import x10.uide.parser.PolyglotNodeLocator;
 
 public class X10ReferenceResolver implements IReferenceResolver, ILanguageService {
     /**
-     * Get the target for a given referencing source node in the AST represented
-     * by a given ParseController.
+     * Get the target for a given source node in the AST represented by a
+     * given Parse Controller.
      */
     public Object getLinkTarget(Object node, IParseController parseController) {
 	if (node instanceof Ambiguous) {
 	    return null;
 	}
-	if (node instanceof Id) {
-	    Id id= (Id) node;
-	    node= findParent(id, parseController);
-	}
 	if (node instanceof TypeNode) {
 	    TypeNode typeNode= (TypeNode) node;
-	    PolyglotNodeLocator locator= (PolyglotNodeLocator) parseController.getNodeLocator();
-	    Node parent= (Node) locator.findParentNode(parseController.getCurrentAst(), typeNode.position().offset(), typeNode.position().endOffset());
-
-	    if (parent instanceof New) {
-		New n= (New) parent;
-		return n.constructorInstance();
-	    }
 	    return typeNode.type();
 	} else if (node instanceof Call) {
 	    Call call= (Call) node;
@@ -64,11 +50,6 @@ public class X10ReferenceResolver implements IReferenceResolver, ILanguageServic
 		return li.declaration();
 	}
 	return node; // If it's not something we know how to resolve, just return the node itself
-    }
-    private Object findParent(Id id, IParseController parseController) {
-	PolyglotNodeLocator locator= (PolyglotNodeLocator) parseController.getNodeLocator();
-
-	return locator.findParentNode(parseController.getCurrentAst(), id.position().offset(), id.position().endOffset());
     }
 
     public static Node findVarDefinition(Local local, Node ast) {
