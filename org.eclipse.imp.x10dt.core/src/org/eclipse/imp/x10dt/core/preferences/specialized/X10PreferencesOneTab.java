e24f910a48a7da82b415336bf29be19e16a07e19
No Author <noone@domain>
2009-06-22 21:57:01 +0000

    This commit was manufactured by cvs2svn to create tag 'Before-17-changes- merged-to-HEAD'.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/tags/Before-17-changes-merged-to-HEAD@10381 a326200e-df2c-42e0-81fa-597e909af41d

--- org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/specialized/X10PreferencesOneTab.java
+++ org.eclipse.imp.x10dt.core/src/org/eclipse/imp/x10dt/core/preferences/specialized/X10PreferencesOneTab.java
@@ -6,37 +6,25 @@
 * http://www.eclipse.org/legal/epl-v10.html
 *
 * Contributors:
-*    Matthew Kaplan (mmk@us.ibm.com) - initial implementation
-*******************************************************************************/
-
-/*******************************************************************************
-* Copyright (c) 2008 IBM Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
 *    Matthew Kaplan (mmk@us.ibm.com) - specialization to show only one tab (to simplify options for novices/typical users)
 *******************************************************************************/
 
 package org.eclipse.imp.x10dt.core.preferences.specialized;
 
-import java.util.Iterator;
-
 import org.eclipse.imp.preferences.IPreferencesService;
+import org.eclipse.imp.preferences.InstancePreferencesTab;
+import org.eclipse.imp.preferences.PreferencesInitializer;
 import org.eclipse.imp.preferences.PreferencesTab;
 import org.eclipse.imp.preferences.TabbedPreferencesPage;
 import org.eclipse.imp.runtime.RuntimePlugin;
-import org.eclipse.imp.x10dt.core.preferences.generated.X10Preferences;
-import org.eclipse.imp.x10dt.core.preferences.generated.X10PreferencesInstanceTab;
+import org.eclipse.imp.x10dt.core.preferences.generated.X10PreferencesInitializer;
 import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.widgets.TabFolder;
 
-public class X10PreferencesOneTab extends X10Preferences implements IPropertyChangeListener{
+public class X10PreferencesOneTab extends TabbedPreferencesPage implements IPropertyChangeListener{
 	
     /** 
      * The first invalid field editor, or <code>null</code>
@@ -46,6 +34,7 @@ public class X10PreferencesOneTab extends X10Preferences implements IPropertyCha
 	
 	public X10PreferencesOneTab () {
 		super();
+        prefService = RuntimePlugin.getInstance().getPreferencesService();
 		setPreferenceStore(RuntimePlugin.getInstance().getPreferenceStore());
 
 	}
@@ -54,13 +43,18 @@ public class X10PreferencesOneTab extends X10Preferences implements IPropertyCha
 			TabbedPreferencesPage page, TabFolder tabFolder) {
 		PreferencesTab[] tabs = new PreferencesTab[1];
 
-		X10PreferencesInstanceTab instanceTab = new X10PreferencesInstanceTabNoDetails(prefService);
-		instanceTab.createInstancePreferencesTab(page, tabFolder);
+		InstancePreferencesTab instanceTab = new X10PreferencesInstanceTabNoDetails(prefService);
+		instanceTab.createTabContents(page, tabFolder);
 		tabs[0] = instanceTab;
 
 		return tabs;
 	}
-	   /**
+
+    public PreferencesInitializer getPreferenceInitializer() {
+        return new X10PreferencesInitializer();
+    }
+
+	/**
      * The field editor preference page implementation of this <code>IPreferencePage</code>
      * (and <code>IPropertyChangeListener</code>) method intercepts <code>IS_VALID</code> 
      * events but passes other events on to its superclass.
@@ -87,9 +81,9 @@ public class X10PreferencesOneTab extends X10Preferences implements IPropertyCha
     protected void checkState() {
     	// Valid if all fields, on all tabs, are valid.  Delegate to each tab and remember first offending field, if any.
         invalidFieldEditor = null;
-        X10PreferencesInstanceTabNoDetails tabs[] = (X10PreferencesInstanceTabNoDetails[])getTabs();
+        PreferencesTab tabs[] = getTabs();
         for (int i=0; i<tabs.length; i++) {
-        	FieldEditor fe = tabs[i].doCheckState();
+        	FieldEditor fe = ((X10PreferencesInstanceTabNoDetails)tabs[i]).doCheckState();
         	if (fe!=null) {
         		invalidFieldEditor = fe;
         		setValid(false);
@@ -98,6 +92,21 @@ public class X10PreferencesOneTab extends X10Preferences implements IPropertyCha
         }
         setValid(true);
      }
+    
+    public boolean performCancel()
+    {
+    	// SMS 4 Dec 2006
+    	// Previously just used to return true; now trying to
+    	// allow for a negative return
+        PreferencesTab tabs[] = getTabs();
+    	boolean result = true;
+		for (int i = 0; i < tabs.length; i++) {
+			if (tabs[i]==null) continue;
+			result = result && tabs[i].performCancel();
+		}
+
+        return result;
+    }
 
 
 }
--- org.eclipse.imp.x10dt.formatter/src/org/eclipse/imp/x10dt/formatter/parser/ParseController.java
+++ org.eclipse.imp.x10dt.formatter/src/org/eclipse/imp/x10dt/formatter/parser/ParseController.java
@@ -7,75 +7,87 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
+
 *******************************************************************************/
 
 package org.eclipse.imp.x10dt.formatter.parser;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.imp.language.ILanguageService;
+import org.eclipse.imp.model.ISourceProject;
+import org.eclipse.imp.parser.ILexer;
+import org.eclipse.imp.parser.IMessageHandler;
 import org.eclipse.imp.parser.IParseController;
+import org.eclipse.imp.parser.IParser;
 import org.eclipse.imp.parser.ISourcePositionLocator;
 import org.eclipse.imp.parser.MessageHandlerAdapter;
 import org.eclipse.imp.parser.SimpleLPGParseController;
 import org.eclipse.imp.services.ILanguageSyntaxProperties;
-import org.eclipse.imp.x10dt.core.X10DTCorePlugin;
+import org.eclipse.imp.x10dt.core.X10Plugin;
 import org.eclipse.imp.x10dt.formatter.parser.ast.ASTNode;
-
-import polyglot.ext.x10.ExtensionInfo;
-import polyglot.ext.x10.ast.X10NodeFactory;
-import polyglot.ext.x10.ast.X10NodeFactory_c;
-import polyglot.ext.x10.types.X10TypeSystem;
-import polyglot.ext.x10.types.X10TypeSystem_c;
-import polyglot.frontend.FileSource;
-import polyglot.frontend.Resource;
-import polyglot.frontend.Source;
-import polyglot.util.ErrorQueue;
-import polyglot.util.SilentErrorQueue;
 
 public class ParseController extends SimpleLPGParseController implements
 		IParseController, ILanguageService {
+	private PatternX10Parser parser;
+	private PatternX10Lexer lexer;
+
+	/**
+	 * @param filePath
+	 *            Absolute path of file
+	 * @param project
+	 *            Project that contains the file
+	 * @param handler
+	 *            A message handler to receive error messages (or any others)
+	 *            from the parser
+	 */
+	public void initialize(IPath filePath, ISourceProject project,
+			IMessageHandler handler) {
+		super.initialize(filePath, project, handler);
+	}
+
+	public IParser getParser() {
+		return parser;
+	}
+
+	public ILexer getLexer() {
+		return lexer;
+	}
+	
 	public ParseController() {
-		super(X10DTCorePlugin.kLanguageName);
+		super(X10Plugin.kLanguageName);
 	}
 
 	/**
 	 * setFilePath() should be called before calling this method.
 	 */
-	public Object parse(String contents, IProgressMonitor monitor) {
+	public Object parse(String contents, boolean scanOnly,
+			IProgressMonitor monitor) {
 		PMMonitor my_monitor = new PMMonitor(monitor);
 		char[] contentsArray = contents.toCharArray();
 
-		if (fLexer == null) {
-			fLexer = new X10Lexer();
-		}
-		fLexer.reset(contentsArray, fFilePath.toOSString());
-		
-		
-		
-		if (fParser == null) {
-			X10TypeSystem ts = new X10TypeSystem_c();
-			ExtensionInfo xi = new ExtensionInfo();
-			X10NodeFactory nf = new X10NodeFactory_c(xi);
-			ErrorQueue eq = new SilentErrorQueue(0,"SilentEQ");
-			Source fs;
-			fs = new Source("") {};
-			fParser = new X10Parser(fLexer.getILexStream(), ts,nf, fs, eq);
-			
-		}
-		
-		fParser.reset(fLexer.getILexStream());
-		fParser.getIPrsStream().setMessageHandler(new MessageHandlerAdapter(handler));
 
-		fLexer.lexer(my_monitor, fParser.getIPrsStream()); // Lex the stream to produce the token stream
+		if (lexer == null) {
+			lexer = new PatternX10Lexer();
+		}
+		lexer.reset(contentsArray, fFilePath.toOSString());
+		
+		if (parser == null) {
+			parser = new PatternX10Parser(lexer.getLexStream());
+		}
+		
+		parser.reset(lexer.getLexStream());
+		parser.getParseStream().setMessageHandler(new MessageHandlerAdapter(handler));
+
+		lexer.lexer(my_monitor, parser.getParseStream()); // Lex the stream to
+															// produce the token
+															// stream
 		if (my_monitor.isCancelled())
 			return fCurrentAst; // TODO fCurrentAst might (probably will) be
 								// inconsistent wrt the lex stream now
 
-		fCurrentAst = fParser.parser(my_monitor, 0);
+		fCurrentAst = (ASTNode) parser.parser(my_monitor, 0);
 
 		cacheKeywordsOnce();
 
@@ -92,4 +104,5 @@ public class ParseController extends SimpleLPGParseController implements
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 }
