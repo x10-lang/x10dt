8f65a55e7736a7c90395b732c204357a1878ed47
Emmanuel Geay <egeay@us.ibm.com>
2010-09-17 14:22:43 +0000

    Added a few more utility methods to create Task markers
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@16506 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui.launch.core/src/x10dt/ui/launch/core/utils/CoreResourceUtils.java
+++ x10dt.ui.launch.core/src/x10dt/ui/launch/core/utils/CoreResourceUtils.java
@@ -29,67 +29,75 @@ import x10dt.ui.launch.core.LaunchCore;
 public final class CoreResourceUtils {
 	
   /**
-   * Adds an X10 Problem marker to the resource and the different characteristics provided.
+   * Adds an X10 Problem marker to the resource with the different parameters provided.
+   * 
+   * <p>This method takes the location from the resource given and provides no line number and start/offset information.
    * 
    * @param resource The resource with which the marker will be associated to.
    * @param msg The marker message.
    * @param severity The marker severity.
-   * @param loc The marker location.
    * @param priority The marker priority.
    */
   public static void addBuildMarkerTo(final IResource resource, final String msg, final int severity, final int priority) {
-    createMarker(X10DTCoreConstants.PROBLEMMARKER_ID, resource, msg, severity, resource.getLocation().toString(), priority, -1, 0, 0);
+    createMarker(X10DTCoreConstants.PROBLEMMARKER_ID, resource, msg, severity, resource.getLocation().toString(), priority, 
+                 -1, 0, 0);
   }
   
   /**
-   * Adds an X10 Problem marker to the resource and the different characteristics provided.
+   * Adds an X10 Problem marker to the resource with the different parameters provided.
+   * 
+   * <p>This method provides no line number and start/offset information.
    * 
    * @param resource The resource with which the marker will be associated to.
    * @param msg The marker message.
    * @param severity The marker severity.
-   * @param loc The marker location.
    * @param priority The marker priority.
+   * @param loc The marker location.
    */
-  public static void addBuildMarkerTo(final IResource resource, final String msg, final int severity, final String loc, 
-                                      final int priority) {
+  public static void addBuildMarkerTo(final IResource resource, final String msg, final int severity, final int priority, 
+                                      final String loc) {
     createMarker(X10DTCoreConstants.PROBLEMMARKER_ID, resource, msg, severity, loc, priority, -1, 0, 0);
   }
   
   /**
-   * Adds an X10 Problem marker to the resource and the different characteristics provided.
+   * Adds an X10 Problem marker to the resource with the different parameters provided.
+   * 
+   * <p>This method provides no start/offset information.
    * 
    * @param resource The resource with which the marker will be associated to.
    * @param msg The marker message.
    * @param severity The marker severity.
-   * @param loc The marker location.
    * @param priority The marker priority.
+   * @param loc The marker location.
    * @param lineNum The marker line number.
    */
-  public static void addBuildMarkerTo(final IResource resource, final String msg, final int severity, final String loc, 
-                                      final int priority, final int lineNum) {
+  public static void addBuildMarkerTo(final IResource resource, final String msg, final int severity, final int priority, 
+                                      final String loc, final int lineNum) {
     createMarker(X10DTCoreConstants.PROBLEMMARKER_ID, resource, msg, severity, loc, priority, lineNum, 0, 0);
   }
   
   /**
-   * Adds an X10 Problem marker to the resource and the different characteristics provided.
+   * Adds an X10 Problem marker to the resource with the different parameters provided.
    * 
    * @param resource The resource with which the marker will be associated to.
    * @param msg The marker message.
    * @param severity The marker severity.
-   * @param loc The marker location.
    * @param priority The marker priority.
+   * @param loc The marker location.
    * @param lineNum The marker line number.
    * @param startOffset The marker start offset.
    * @param endOffset The marker end offset.
    */
-  public static void addBuildMarkerTo(final IResource resource, final String msg, final int severity, final String loc, 
-                                      final int priority, final int lineNum, final int startOffset, 
+  public static void addBuildMarkerTo(final IResource resource, final String msg, final int severity, final int priority, 
+                                      final String loc, final int lineNum, final int startOffset, 
                                       final int endOffset) {
   	createMarker(X10DTCoreConstants.PROBLEMMARKER_ID, resource, msg, severity, loc, priority, lineNum, startOffset, endOffset);
   }
   
   /**
    * Adds a marker for a platform configuration file with the different parameters provided.
+   * 
+   * <p>This method takes the location from the resource given and provides no line number and start/offset information.
    * 
    * @param file The platform configuration file to consider.
    * @param msg The The marker message.
@@ -103,6 +111,24 @@ public final class CoreResourceUtils {
   
   /**
    * Adds a marker for a platform configuration file with the different parameters provided.
+   * 
+   * p>This method provides no line number and start/offset information.
+   * 
+   * @param file The platform configuration file to consider.
+   * @param msg The The marker message.
+   * @param severity The marker severity.
+   * @param priority The marker priority.
+   * @param loc The marker location.
+   */
+  public static void addPlatformConfMarker(final IFile file, final String msg, final int severity, final int priority, 
+                                           final String loc) {
+    createMarker(PLATFORM_CONF_MARKER_ID, file, msg, severity, loc, priority, -1, 0, 0);
+  }
+  
+  /**
+   * Adds a marker for a platform configuration file with the different parameters provided.
+   * 
+   * <p>This method provides no start/offset information.
    * 
    * @param file The platform configuration file to consider.
    * @param msg The The marker message.
@@ -135,31 +161,115 @@ public final class CoreResourceUtils {
   }
   
   /**
-   * Deletes all the X10 build markers for a given project.
+   * Adds a task marker to the resource provided with the help of parameters provided.
    * 
-   * @param project The project to consider.
+   * <p>This method takes the location from the resource given and provides no line number and start/offset information.
+   * 
+   * @param resource The resource with which the marker will be associated to.
+   * @param msg The The marker message.
+   * @param severity The marker severity.
+   * @param priority The marker priority.
    */
-  public static void deleteBuildMarkers(final IProject project) {
-  	deleteMarker(X10DTCoreConstants.PROBLEMMARKER_ID, project, true /* includeSubtypes */, IResource.DEPTH_INFINITE);
+  public static void addTask(final IResource resource, final String msg, final int severity, final int priority) {
+    createMarker(IMarker.TASK, resource, msg, severity, resource.getLocation().toString(), priority, -1, 0, 0);
   }
   
   /**
-   * Deletes all the X10 build markers for a given file.
+   * Adds a task marker to the resource provided with the help of parameters provided.
+   * 
+   * <p>This method provides no line number and start/offset information.
+   * 
+   * @param resource The resource with which the marker will be associated to.
+   * @param msg The The marker message.
+   * @param severity The marker severity.
+   * @param priority The marker priority.
+   * @param loc The marker location.
+   */
+  public static void addTask(final IResource resource, final String msg, final int severity, final int priority,
+                             final String loc) {
+    createMarker(IMarker.TASK, resource, msg, severity, loc, priority, -1, 0, 0);
+  }
+  
+  /**
+   * Adds a task marker to the resource provided with the help of parameters provided.
+   * 
+   * <p>This method provides no start/offset information.
+   * 
+   * @param resource The resource with which the marker will be associated to.
+   * @param msg The The marker message.
+   * @param severity The marker severity.
+   * @param priority The marker priority.
+   * @param loc The marker location.
+   * @param lineNum The marker line number.
+   */
+  public static void addTask(final IResource resource, final String msg, final int severity, final int priority, 
+                             final String loc, final int lineNum) {
+    createMarker(IMarker.TASK, resource, msg, severity, loc, priority, lineNum, 0, 0);
+  }
+  
+  /**
+   * Adds a task marker to the resource provided with the help of parameters provided.
+   * 
+   * @param resource The resource with which the marker will be associated to.
+   * @param msg The The marker message.
+   * @param severity The marker severity.
+   * @param priority The marker priority.
+   * @param loc The marker location.
+   * @param lineNum The marker line number.
+   * @param startOffset The marker start offset.
+   * @param endOffset The marker end offset.
+   */
+  public static void addTask(final IResource resource, final String msg, final int severity, final String loc, 
+                             final int priority, final int lineNum, final int startOffset, 
+                             final int endOffset) {
+    createMarker(IMarker.TASK, resource, msg, severity, loc, priority, lineNum, startOffset, endOffset);
+  }
+  
+  /**
+   * Deletes the X10 build markers and its sub-markers for the particular depth for the transmitted project.
+   * 
+   * @param project The project to consider.
+   * @param depth The markers tree depth for the project consider.
+   */
+  public static void deleteBuildMarkers(final IProject project, final int depth) {
+  	deleteMarkers(X10DTCoreConstants.PROBLEMMARKER_ID, project, true /* includeSubtypes */, depth);
+  }
+  
+  /**
+   * Deletes all the X10 build markers for a given file. No recursion in the possible tree of markers will occur.
    * 
    * @param file The file to consider.
    */
   public static void deleteBuildMarkers(final IFile file) {
-  	deleteMarker(X10DTCoreConstants.PROBLEMMARKER_ID, file, false /* includeSubtypes */, IResource.DEPTH_ZERO);
+  	deleteMarkers(X10DTCoreConstants.PROBLEMMARKER_ID, file, false /* includeSubtypes */, IResource.DEPTH_ZERO);
   }
   
-  
   /**
-   * Deletes all the X10 platform configuration markers for a given project.
+   * Deletes all the X10 platform configuration markers for the given file.
    * 
    * @param file The platform configuration file to consider.
    */
   public static void deletePlatformConfMarkers(final IFile file) {
-  	deleteMarker(PLATFORM_CONF_MARKER_ID, file, false /* includeSubtypes */, IResource.DEPTH_ZERO);
+  	deleteMarkers(PLATFORM_CONF_MARKER_ID, file, false /* includeSubtypes */, IResource.DEPTH_ZERO);
+  }
+  
+  /**
+   * Deletes all the tasks and its sub-markers for the particular depth for the transmitted resource.
+   * 
+   * @param resource The resource to consider.
+   * @param depth The markers tree depth for the resource considered.
+   */
+  public static void deleteTasks(final IResource resource, final int depth) {
+    deleteMarkers(IMarker.TASK, resource, true, depth);
+  }
+  
+  /**
+   * Deletes all the tasks for the given resource. No recursion in the possible tree of markers will occur.
+   * 
+   * @param resource The resource to consider.
+   */
+  public static void deleteTasks(final IResource resource) {
+    deleteMarkers(IMarker.TASK, resource, false, IResource.DEPTH_ZERO);
   }
   
   /**
@@ -226,36 +336,39 @@ public final class CoreResourceUtils {
 		}
   }
   
-  public static void deleteMarker(final String markerId, final IResource resource, final boolean includeSubtypes, final int depth){
-	  try {
-	  	  final IWorkspace workspace = ResourcesPlugin.getWorkspace();
-	  	  final ISchedulingRule rule = workspace.getRuleFactory().markerRule(resource);
-	  	  
-	  	  final IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
-	        
-	        public void run(final IProgressMonitor monitor) throws CoreException {
-	        	resource.deleteMarkers(markerId, includeSubtypes, depth);
-	        }
-	       
-	      };
-				
-	      workspace.run(runnable, rule, IWorkspace.AVOID_UPDATE, null);
-	  } catch (CoreException except) {
-		  LaunchCore.log(except.getStatus());
-	  }
-  }
+  // --- Private code
   
-  public static void createMarker(final String markerId, final IResource resource, final String msg, final int severity,
-                                   final String loc, final int priority, final int lineNum, final int startOffset, 
-                                   final int endOffset) {
-  	try {
-  	  final IWorkspace workspace = ResourcesPlugin.getWorkspace();
-  	  final ISchedulingRule rule = workspace.getRuleFactory().markerRule(resource);
-  	  
-  	  final IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
-        
+  private static void deleteMarkers(final String markerId, final IResource resource, final boolean includeSubtypes,
+                                    final int depth) {
+    try {
+      final IWorkspace workspace = ResourcesPlugin.getWorkspace();
+      final ISchedulingRule rule = workspace.getRuleFactory().markerRule(resource);
+
+      final IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
+
         public void run(final IProgressMonitor monitor) throws CoreException {
-          final IMarker marker = resource.createMarker(markerId); 
+          resource.deleteMarkers(markerId, includeSubtypes, depth);
+        }
+
+      };
+
+      workspace.run(runnable, rule, IWorkspace.AVOID_UPDATE, null);
+    } catch (CoreException except) {
+      LaunchCore.log(except.getStatus());
+    }
+  }
+
+  private static void createMarker(final String markerId, final IResource resource, final String msg, final int severity,
+                                   final String loc, final int priority, final int lineNum, final int startOffset,
+                                   final int endOffset) {
+    try {
+      final IWorkspace workspace = ResourcesPlugin.getWorkspace();
+      final ISchedulingRule rule = workspace.getRuleFactory().markerRule(resource);
+
+      final IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
+
+        public void run(final IProgressMonitor monitor) throws CoreException {
+          final IMarker marker = resource.createMarker(markerId);
 
           marker.setAttribute(IMarker.MESSAGE, msg);
           marker.setAttribute(IMarker.SEVERITY, severity);
@@ -267,15 +380,15 @@ public final class CoreResourceUtils {
           if (startOffset >= 0) {
             marker.setAttribute(IMarker.CHAR_START, startOffset);
             marker.setAttribute(IMarker.CHAR_END, endOffset + 1);
-          }          
+          }
         }
-        
+
       };
-			
+
       workspace.run(runnable, rule, IWorkspace.AVOID_UPDATE, null);
-		} catch (CoreException except) {
-			LaunchCore.log(except.getStatus());
-		}
+    } catch (CoreException except) {
+      LaunchCore.log(except.getStatus());
+    }
   }
   
   // --- Fields
