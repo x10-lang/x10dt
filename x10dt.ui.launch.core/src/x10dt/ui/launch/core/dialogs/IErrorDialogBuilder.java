dc1a4b1fbc70a9f248e5d0bbb0c45f31b260943e
Emmanuel Geay <egeay@us.ibm.com>
2010-09-17 14:21:25 +0000

    Javadoc update
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@16504 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui.launch.core/src/x10dt/ui/launch/core/dialogs/IErrorDialogBuilder.java
+++ x10dt.ui.launch.core/src/x10dt/ui/launch/core/dialogs/IErrorDialogBuilder.java
@@ -9,8 +9,10 @@ package x10dt.ui.launch.core.dialogs;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.window.IShellProvider;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Shell;
 
