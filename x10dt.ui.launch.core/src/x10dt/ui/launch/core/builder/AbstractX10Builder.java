d62479624905a5ab4e6ad5076d06349cb39ce6bb
Mandana Vaziri <mvaziri@us.ibm.com>
2010-10-01 22:01:21 +0000

    Added support for inclusion/exclusion patterns in the build path (X10DT).
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@16867 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui.launch.core/src/x10dt/ui/launch/core/builder/AbstractX10Builder.java
+++ x10dt.ui.launch.core/src/x10dt/ui/launch/core/builder/AbstractX10Builder.java
@@ -45,6 +45,7 @@ import org.eclipse.imp.preferences.IPreferencesService;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.console.MessageConsoleStream;
@@ -177,6 +178,7 @@ public abstract class AbstractX10Builder extends IncrementalProjectBuilder {
       final Set<IProject> dependentProjects = new HashSet<IProject>();
       collectSourceFilesToCompile(sourcesToCompile, nativeFiles, deletedSources, this.fProjectWrapper, dependentProjects,
                                   createNativeFilesFilter(), shouldBuildAll, subMonitor.newChild(5));
+      filter(sourcesToCompile);
       if (subMonitor.isCanceled()) {
         return null;
       }
@@ -315,6 +317,130 @@ public abstract class AbstractX10Builder extends IncrementalProjectBuilder {
 
   // --- Private code
 
+  private void filter(Collection<IFile> files) throws JavaModelException {
+	  Collection<IFile> filesToRemove = new ArrayList<IFile>();
+	  for (IFile file: files){
+		  // --- Determine the classpath source entry that corresponds to file. 
+		  IClasspathEntry srcEntry = null;
+		  for (final IClasspathEntry cpEntry : this.fProjectWrapper.getRawClasspath()) {
+			  if (cpEntry.getEntryKind() == IClasspathEntry.CPE_SOURCE && cpEntry.getPath().isPrefixOf(file.getFullPath())){
+				  srcEntry = cpEntry;
+				  break;
+			  }
+		  }  
+		  IPath filePath = file.getFullPath().makeRelativeTo(srcEntry.getPath());
+		  IPath[] inclusionPatterns = srcEntry.getInclusionPatterns();
+		  if (inclusionPatterns != null && inclusionPatterns.length != 0){
+			  boolean foundMatch = false;
+			  for(IPath pattern: inclusionPatterns){
+				  if (matches(filePath, pattern)){
+					  foundMatch = true;
+					  break;
+				  }
+			  }
+			  if (!foundMatch){
+				  filesToRemove.add(file);
+			  }
+		  }
+		  IPath[] exclusionPatterns = srcEntry.getExclusionPatterns();
+		  if (exclusionPatterns != null && exclusionPatterns.length != 0){
+			  for(IPath pattern: exclusionPatterns){
+				  if (matches(filePath, pattern)){
+					  filesToRemove.add(file);
+				  }
+			  }
+		  }
+	  }
+	  files.removeAll(filesToRemove);
+  }
+  
+  /**
+   * This method takes a file and a pattern and determines if there is a match.
+   * The patterns have the same format as ANT patterns:
+   * - '*' matches zero or more characters.
+   * - '?' matches one character.
+   * - '/' is used to separate folders: This means the first segment in the pattern is matched against the most outer folder name in the path to match, the second segment with the second, and so on.
+   * - '**' matches any number of folders.
+   * 
+   * @param file The file we are matching.
+   * @param pattern The pattern to match.
+   * @return true if there is a match.
+   */
+  private boolean matches(IPath path, IPath pattern){
+	if (path.equals(pattern)){
+		return true;
+	}
+	if (pattern.segmentCount() == 1 && pattern.segment(0).equals("**")){
+		return true;
+	}
+	if (path.isEmpty() && !pattern.isEmpty()){ // --- In this case, there is a match only if pattern is of the form **/**/**...
+		if (!pattern.segment(0).equals("**"))
+			return false;
+		return matches(path, pattern.removeFirstSegments(1));
+	}
+	if (!path.isEmpty() && pattern.isEmpty()){ 
+		return false;
+	}
+	String pathFirst = path.segment(0);
+	String patternFirst = pattern.segment(0);
+	if (patternFirst.equals("**")){
+		IPath patternNext = pattern.removeFirstSegments(1);
+		for (int i = 0; i < path.segmentCount(); i++){
+			if (matches(path.removeFirstSegments(i), patternNext)){
+				return true;
+			}
+		}
+		return false;
+	} else {
+		if (!segMatches(pathFirst, patternFirst)){
+			return false;
+		}
+		return matches(path.removeFirstSegments(1), pattern.removeFirstSegments(1));
+	}	
+  }
+  
+  /**
+   * This method determines if 2 string segments match. We assume that the pattern does not contain '**'.
+   * 
+   * @param seg The segment to match
+   * @param pattern The pattern to match
+   * @return true if the segment and pattern match
+   */
+  private boolean segMatches(String seg, String pattern){
+	  if (seg.equals(pattern) || pattern.equals("*")){
+		  return true;
+	  }
+	  if (seg.equals("") && !pattern.equals("")){ // --- In this case, there is a match only if pattern is of the form ***...
+		  if (pattern.charAt(0) != '*'){
+			  return false;
+		  }
+		  return segMatches(seg, pattern.substring(1));
+	  }
+	  if (!seg.equals("") && pattern.equals("")){ 
+		  return false;
+	  }
+	  char patternFirst = pattern.charAt(0);
+	  char segFirst = seg.charAt(0);
+	  if (patternFirst == '*'){
+		  String patternNext = pattern.substring(1);
+		  for(int i = 0; i < seg.length(); i++){
+			  if (segMatches(seg.substring(i), patternNext)){
+				  return true;
+			  }
+		  }
+		  return false;
+	  }
+	  if (patternFirst == '?'){
+		  return segMatches(seg.substring(1), pattern.substring(1));
+	  } else {
+		  if (patternFirst != segFirst){
+			  return false;
+		  }
+		  return segMatches(seg.substring(1), pattern.substring(1));
+	  }
+	  
+  }
+  
   private ISchedulingRule getSchedulingRule(Collection<IFile> files){
 	  Collection<ISchedulingRule> rules = new ArrayList<ISchedulingRule>();
 	  for(IFile file: files){
