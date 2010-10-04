f3260d8ec5a22bcbbd6272e0099b64ab02726d5f
Adam Taylor <awtaylor@us.ibm.com>
2010-10-04 23:26:07 +0000

    Another compile fix for r16948
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@16964 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui/src/x10dt/ui/editor/X10EditorActionContributer.java
+++ x10dt.ui/src/x10dt/ui/editor/X10EditorActionContributer.java
@@ -1,52 +1,39 @@
 package x10dt.ui.editor;
 
 import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchActionConstants;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.editors.text.TextEditorActionContributor;
-import org.eclipse.ui.texteditor.ITextEditor;
-import org.eclipse.ui.texteditor.RetargetTextEditorAction;
-
-import x10dt.ui.Messages;
 
 public class X10EditorActionContributer extends TextEditorActionContributor {
 	public static final String OPEN_HIERARCHY= "x10dt.ui.navigate.OpenHierarchy"; //$NON-NLS-1$
 
-	private RetargetTextEditorAction fOpenHierarchy;
+//	private RetargetTextEditorAction fOpenHierarchy;
 	
 	public X10EditorActionContributer()
 	{
-		fOpenHierarchy= new RetargetTextEditorAction(Messages.getBundleForConstructedKeys(), "OpenHierarchy."); //$NON-NLS-1$
-		fOpenHierarchy.setActionDefinitionId(OPEN_HIERARCHY);
+//		fOpenHierarchy= new RetargetTextEditorAction(Messages.getBundleForConstructedKeys(), "OpenHierarchy."); //$NON-NLS-1$
+//		fOpenHierarchy.setActionDefinitionId(OPEN_HIERARCHY);
 	}
 	
 	@Override
 	public void contributeToMenu(IMenuManager menu) {
 		super.contributeToMenu(menu);
 		
-		IMenuManager navigateMenu= menu.findMenuUsingPath(IWorkbenchActionConstants.M_NAVIGATE);
-		if (navigateMenu != null) {
-			navigateMenu.appendToGroup(IWorkbenchActionConstants.SHOW_EXT, fOpenHierarchy);
-		}
-	}
-
-	@Override
-	public void init(IActionBars bars, IWorkbenchPage page) {
-		// TODO Auto-generated method stub
-		super.init(bars, page);
+//		IMenuManager navigateMenu= menu.findMenuUsingPath(IWorkbenchActionConstants.M_NAVIGATE);
+//		if (navigateMenu != null) {
+//			navigateMenu.appendToGroup(IWorkbenchActionConstants.SHOW_EXT, fOpenHierarchy);
+//		}
 	}
 
 	@Override
 	public void setActiveEditor(IEditorPart part) {
 		super.setActiveEditor(part);
 		
-		ITextEditor textEditor= null;
-		if (part instanceof ITextEditor)
-			textEditor= (ITextEditor)part;
-		
-		fOpenHierarchy.setAction(getAction(textEditor, OPEN_HIERARCHY));
+//		ITextEditor textEditor= null;
+//		if (part instanceof ITextEditor)
+//			textEditor= (ITextEditor)part;
+//		
+//		fOpenHierarchy.setAction(getAction(textEditor, OPEN_HIERARCHY));
 	}
 	
 }
