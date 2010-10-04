f9d2fd19cd4cb2a2fae2851caf42aea5bdfc1ff7
Adam Taylor <awtaylor@us.ibm.com>
2010-10-04 21:20:25 +0000

    Fixes incorrect editor icons JIRA XTENLANG-1101 RTC 197
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@16948 a326200e-df2c-42e0-81fa-597e909af41d

---
+++ x10dt.ui/src/x10dt/ui/editor/X10Editor.java
@@ -0,0 +1,94 @@
+package x10dt.ui.editor;
+
+import org.eclipse.imp.editor.UniversalEditor;
+import org.eclipse.imp.ui.DefaultPartListener;
+import org.eclipse.jdt.internal.ui.javaeditor.JavaSourceViewer;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.search.ui.IContextMenuConstants;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.texteditor.TextOperationAction;
+
+import x10dt.ui.Messages;
+import x10dt.ui.X10DTUIPlugin;
+
+public class X10Editor extends UniversalEditor {
+	private DefaultPartListener fRefreshContributions;
+
+	public static final String TYPE_ACTION_SET = X10DTUIPlugin.PLUGIN_ID
+			+ ".typeActionSet";
+
+	public X10Editor() {
+		super();
+		setSourceViewerConfiguration(new X10StructuredSourceViewerConfiguration(
+				X10DTUIPlugin.getInstance().getPreferenceStore(), this));
+	}
+
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+		registerEditorContributionsActivator();
+	}
+
+	@Override
+	public void dispose() {
+		unregisterEditorContributionsActivator();
+		super.dispose();
+	}
+
+	/**
+	 * Makes sure that menu items and status bar items disappear as the editor
+	 * is out of focus, and reappear when it gets focus again. This does not
+	 * work for toolbar items for unknown reasons, they stay visible.
+	 * 
+	 */
+	private void registerEditorContributionsActivator() {
+		fRefreshContributions = new DefaultPartListener() {
+			@Override
+			public void partActivated(IWorkbenchPart part) {
+				if (part instanceof UniversalEditor) {
+					part.getSite().getPage().showActionSet(TYPE_ACTION_SET);
+				} else {
+					part.getSite().getPage().hideActionSet(TYPE_ACTION_SET);
+				}
+			}
+		};
+		getSite().getPage().addPartListener(fRefreshContributions);
+	}
+
+	private void unregisterEditorContributionsActivator() {
+		if (fRefreshContributions != null) {
+			getSite().getPage().removePartListener(fRefreshContributions);
+		}
+		fRefreshContributions = null;
+	}
+
+	@Override
+	protected void createActions() {
+		super.createActions();
+
+		IAction action = new TextOperationAction(Messages
+				.getBundleForConstructedKeys(),
+				"OpenHierarchy.", this, JavaSourceViewer.SHOW_HIERARCHY, true); //$NON-NLS-1$
+		action.setActionDefinitionId(X10EditorActionContributer.OPEN_HIERARCHY);
+		setAction(X10EditorActionContributer.OPEN_HIERARCHY, action);
+		// PlatformUI.getWorkbench().getHelpSystem().setHelp(action,
+		// IJavaHelpContextIds.OPEN_HIERARCHY_ACTION);
+	}
+
+	@Override
+	protected void editorContextMenuAboutToShow(IMenuManager menu) {
+		super.editorContextMenuAboutToShow(menu);
+
+		// Quick views
+		IAction action = getAction(X10EditorActionContributer.OPEN_HIERARCHY);
+		menu.appendToGroup(IContextMenuConstants.GROUP_OPEN, action);
+	}
+
+	/*
+	 * @see org.eclipse.ui.texteditor.AbstractDecoratedTextEditor#initializeKeyBindingScopes()
+	 */
+	protected void initializeKeyBindingScopes() {
+		setKeyBindingScopes(new String[] { "x10dt.ui.x10EditorScope" });  //$NON-NLS-1$
+	}
+}
