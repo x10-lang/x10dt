ae47f2b8104211331a49ff60f69c574dda60256f
Robert Fuhrer <rfuhrer@us.ibm.com>
2010-11-11 23:38:12 +0000

    Restored the logic to remove the JDT builder from the set of build commands, for as long as we're still dependent on the JDT project nature.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@18211 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui.launch.java/src/x10dt/ui/launch/java/nature/X10ProjectNature.java
+++ x10dt.ui.launch.java/src/x10dt/ui/launch/java/nature/X10ProjectNature.java
@@ -7,28 +7,26 @@
 *
 * Contributors:
 *    Robert Fuhrer (rfuhrer@watson.ibm.com) - initial API and implementation
-
 *******************************************************************************/
 
-/*
- * (C) Copyright IBM Corporation 2007
- * 
- * This file is part of the Eclipse IMP.
- */
 package x10dt.ui.launch.java.nature;
 
+import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.imp.builder.ProjectNatureBase;
 import org.eclipse.imp.runtime.IPluginLog;
 import org.eclipse.imp.smapifier.builder.SmapiProjectNature;
+import org.eclipse.jdt.core.JavaCore;
 
 import x10dt.core.X10DTCorePlugin;
 import x10dt.ui.launch.core.LaunchCore;
 import x10dt.ui.launch.java.Activator;
 
 public class X10ProjectNature extends ProjectNatureBase {
-    
+
     public String getNatureID() {
         return LaunchCore.X10_PRJ_JAVA_NATURE_ID;
     }
@@ -51,6 +49,29 @@ public class X10ProjectNature extends ProjectNatureBase {
     @Override
     public void configure() throws CoreException {
       super.configure();
+      final IProject project = getProject();
+      final IProjectDescription description = project.getDescription();
+      final ICommand[] commands = description.getBuildSpec();
+      if (hasJavaBuilder(commands)) {
+        final ICommand[] newCommands = new ICommand[commands.length - 1];
+        int k = -1;
+        for(int i = 0; i < commands.length; i++){
+          if (!commands[i].getBuilderName().equals(JavaCore.BUILDER_ID)){
+            newCommands[++k] = commands[i];
+          }
+        }
+        description.setBuildSpec(newCommands);
+        project.setDescription(description, new NullProgressMonitor());
+      }
     }
     
+    private boolean hasJavaBuilder(final ICommand[] commands) {
+      for (final ICommand command : commands) {
+        if (JavaCore.BUILDER_ID.equals(command.getBuilderName())) {
+          return true;
+        }
+      }
+      return false;
+    }
+
 }
