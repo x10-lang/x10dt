3572d74cd6823cc259a6614232777a70cda415cf
Robert Fuhrer <rfuhrer@us.ibm.com>
2011-01-06 22:08:56 +0000

    Refactored to remove undesirable code duplication. Now brings up the quick outline several times in case there were severe memory leaks.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@19319 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui.tests/src/x10dt/ui/editor/QuickOutlineTests.java
+++ x10dt.ui.tests/src/x10dt/ui/editor/QuickOutlineTests.java
@@ -11,10 +11,9 @@
 
 package x10dt.ui.editor;
 
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.imp.editor.UniversalEditor;
 import org.eclipse.jface.bindings.keys.KeyStroke;
+import org.eclipse.jface.bindings.keys.ParseException;
 import org.eclipse.swtbot.eclipse.finder.waits.Conditions;
 import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
@@ -39,12 +38,17 @@ import x10dt.ui.tests.utils.EditorMatcher;
  */
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class QuickOutlineTests extends X10DTEditorTestBase {
-	static int testID = 0;
+  private static int testID = 0;
+
   private static final String PROJECT_NAME = "TestOutline";
 
   private static final String CLASS_NAME = "Hello1";
 
   private static final String SRC_FILE_NAME = CLASS_NAME + ".x10";
+
+  private static final String MAIN_SIGNATURE= "main(Array[String])";
+
+  private static final String MAIN_SIGNATURE_ELABORATED= "main(x10.array.Array[x10.lang.String])";
 
   @BeforeClass
   public static void beforeClass() throws Exception {
@@ -73,8 +77,10 @@ public class QuickOutlineTests extends X10DTEditorTestBase {
   
   @Test
   public void test1() throws Exception {
-	createJavaBackEndProject(PROJECT_NAME + testID++, false);  
-    ProjectUtils.createClass(topLevelBot, CLASS_NAME);
+	String projName= PROJECT_NAME + testID++;
+
+	createJavaBackEndProject(projName, false);  
+    ProjectUtils.createClass(topLevelBot, CLASS_NAME, projName + "/src", false);
     topLevelBot.waitUntil(Conditions.waitForEditor(new EditorMatcher(SRC_FILE_NAME)));
 
     fSrcEditor = topLevelBot.activeEditor().toTextEditor();
@@ -83,119 +89,83 @@ public class QuickOutlineTests extends X10DTEditorTestBase {
     final IEditorPart editorPart = fSrcEditor.getReference().getEditor(false);
     final UniversalEditor univEditor = (UniversalEditor) editorPart;
 
-    fUpdateListener= new UpdateListener();
     univEditor.addModelListener(fUpdateListener);
+
     part1();
-    part2();
+    for(int i=0; i < 25; i++) {
+        part2(i);
+    }
   }
 
   private void part1() throws Exception {
-    fSrcEditor.setFocus();
-    fSrcEditor.insertText(1, 0, "public static def main(Array[String]) {}\n");
-    fSrcEditor.insertText(2, 0, "public static def foo() { }\n");
-
-    waitForParser();
-    fSrcEditor.save();
-    waitForBuildToFinish(); // WHY???
-
-    try {
-    	SWTBot outlineBot = invokeQuickOutline(topLevelBot);// TODO
-
-    	SWTBotTreeItem classItem = outlineBot.tree().getTreeItem(CLASS_NAME);
-    	SWTBotTreeItem mainItem = classItem.getNode("main(x10.array.Array[x10.lang.String])");
-
-    	classItem.getNode("foo()");
-    	mainItem.doubleClick();
-    } catch (WidgetNotFoundException e){
-    	SWTBot outlineBot = invokeQuickOutline(topLevelBot);// TODO
-
-    	SWTBotTreeItem classItem = outlineBot.tree().getTreeItem(CLASS_NAME);
-    	SWTBotTreeItem mainItem = classItem.getNode("main(x10.array.Array[x10.lang.String])");
-
-    	classItem.getNode("foo()");
-    	mainItem.doubleClick();
-    }
-    
-    Position cursorPos = fSrcEditor.cursorPosition();
-
-    junit.framework.Assert.assertEquals("Cursor positioned at incorrect line after quick outline item selection", 1,
-                                        cursorPos.line);
-    junit.framework.Assert.assertEquals("Cursor positioned at incorrect column after quick outline item selection", 1,
-                                        cursorPos.column);
+    insertMethodsAndNavigate(MAIN_SIGNATURE, MAIN_SIGNATURE_ELABORATED, "foo()", "foo()", 1);
   }
 
-  private void part2() throws Exception {
-    fUpdateListener.reset();
+  private void part2(int idx) throws Exception {
+    String method1Signature = "bar" + idx + "()";
+    String method2Signature = "bletch" + idx + "()";
+    int insertLine = 3 + 2 * idx;
 
-    fSrcEditor.setFocus();
-    // topLevelBot.activeShell().setFocus();
-    fSrcEditor.insertText(3, 0, "public static def bar() { }\n");
-    fSrcEditor.insertText(4, 0, "public static def bletch() { }\n");
-
-    // a better way of determining when the AST has been updated
-    waitForParser();
-    fSrcEditor.save();
-    waitForBuildToFinish(); // WHY???
-
-    try {
-    	SWTBot outlineBot = invokeQuickOutline(topLevelBot);
-
-    	SWTBotTreeItem classItem = outlineBot.tree().getTreeItem(CLASS_NAME);
-
-    	classItem.getNode("bar()");
-
-    	SWTBotTreeItem bletchItem = classItem.getNode("bletch()");
-
-    	bletchItem.doubleClick();
-    } catch (WidgetNotFoundException e){
-    	SWTBot outlineBot = invokeQuickOutline(topLevelBot); //OK to repeat because this resets the focus to editor.
-
-    	SWTBotTreeItem classItem = outlineBot.tree().getTreeItem(CLASS_NAME);
-
-    	classItem.getNode("bar()");
-
-    	SWTBotTreeItem bletchItem = classItem.getNode("bletch()");
-
-    	bletchItem.doubleClick();
-    }
-
-    Position cursorPos = fSrcEditor.cursorPosition();
-
-    junit.framework.Assert.assertEquals("Cursor positioned at incorrect line after quick outline item selection", 4,
-                                        cursorPos.line);
-    junit.framework.Assert.assertEquals("Cursor positioned at incorrect column after quick outline item selection", 1,
-                                        cursorPos.column);
+    insertMethodsAndNavigate(method1Signature, method1Signature, method2Signature, method2Signature, insertLine);
   }
 
-  /**
-   * This method waits for a build to finish before continuing
-   * 
-   * @throws Exception
-   */
-  private void waitForBuildToFinish() throws Exception {
-    Job.getJobManager().join(ResourcesPlugin.FAMILY_AUTO_BUILD, null);
+  private void insertMethodsAndNavigate(String method1Signature, String method1ElaboratedSignature,
+                                        String method2Signature, String method2ElaboratedSignature, int insertLine) throws Exception {
+      fUpdateListener.reset();
+
+      fSrcEditor.setFocus();
+      fSrcEditor.insertText(insertLine,   0, "public static def " + method1Signature + " { }\n");
+      fSrcEditor.insertText(insertLine+1, 0, "public static def " + method2Signature + " { }\n");
+
+      waitForParser();
+      fSrcEditor.navigateTo(0, 0); // Go to the beginning so that quick-outline navigation has to move the cursor
+
+      try {
+          SWTBot outlineBot = invokeQuickOutline(topLevelBot);
+          SWTBotTreeItem classItem = outlineBot.tree().getTreeItem(CLASS_NAME);
+          SWTBotTreeItem meth2Item = classItem.getNode(method2ElaboratedSignature);
+
+          classItem.getNode(method1ElaboratedSignature);
+          meth2Item.doubleClick();
+      } catch (WidgetNotFoundException e) {
+          SWTBot outlineBot = invokeQuickOutline(topLevelBot); // OK to repeat because this resets the focus to editor.
+          SWTBotTreeItem classItem = outlineBot.tree().getTreeItem(CLASS_NAME);
+          SWTBotTreeItem meth2Item = classItem.getNode(method2ElaboratedSignature);
+
+          classItem.getNode(method1ElaboratedSignature);
+          meth2Item.doubleClick();
+      }
+
+      Position cursorPos = fSrcEditor.cursorPosition();
+
+      junit.framework.Assert.assertEquals("Cursor positioned at incorrect line after quick outline item selection",
+                                          insertLine + 1, cursorPos.line);
+      junit.framework.Assert.assertEquals("Cursor positioned at incorrect column after quick outline item selection",
+                                          1, cursorPos.column);
   }
 
   private SWTBot invokeQuickOutline(SWTBot bot) throws Exception {
-	fSrcEditor.setFocus(); //Needed to make this method repeatable.
-	if (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0){  
-		fSrcEditor.pressShortcut(Keystrokes.COMMAND, KeyStroke.getInstance("o"));
-	} else {
-		fSrcEditor.pressShortcut(Keystrokes.CTRL, KeyStroke.getInstance("o"));
-	}
+	fSrcEditor.setFocus(); // Needed to make this method repeatable.
+	submitQuickOutlineShortcut();
+
 	SWTBotShell outlineShell = null;
+
 	try {
 		outlineShell = topLevelBot.shell(WizardConstants.QUICK_OUTLINE_SHELL); // TODO Fix this
-	} catch (WidgetNotFoundException e){
-    	if (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0){  
-    		fSrcEditor.pressShortcut(Keystrokes.COMMAND, KeyStroke.getInstance("o"));
-    	} else {
-    		fSrcEditor.pressShortcut(Keystrokes.CTRL, KeyStroke.getInstance("o"));
-    	}
+	} catch (WidgetNotFoundException e) {
+    	submitQuickOutlineShortcut();
     	outlineShell = topLevelBot.shell(WizardConstants.QUICK_OUTLINE_SHELL);
     }
     outlineShell.activate();
 
     return outlineShell.bot();
+  }
+
+  private void submitQuickOutlineShortcut() throws ParseException {
+    if (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0) {
+		fSrcEditor.pressShortcut(Keystrokes.COMMAND, KeyStroke.getInstance("o"));
+	} else {
+		fSrcEditor.pressShortcut(Keystrokes.CTRL, KeyStroke.getInstance("o"));
+	}
   }
 }
