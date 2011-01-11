7ddbfaff611e2443a0c87de3d807f649413acebf
Rick Lesniak <lesniakr@domain>
2011-01-11 20:17:51 +0000

    Added more SWTBot widget constant strings
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@19389 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.tests.services/src/x10dt/tests/services/swbot/constants/WizardConstants.java
+++ x10dt.tests.services/src/x10dt/tests/services/swbot/constants/WizardConstants.java
@@ -22,7 +22,13 @@ public final class WizardConstants {
   public static final String YES_BUTTON = "Yes"; //$NON-NLS-1$
   
   public static final String OK_BUTTON = "OK"; //$NON-NLS-1$
+
+  public static final String YES_TO_ALL_BUTTON = "Yes To All"; //$NON-NLS-1$
+
+  public static final String NEXT_BUTTON = "Next >"; //$NON-NLS-1$
   
+  public static final String CANCEL_BUTTON = "Cancel"; //$NON-NLS-1$
+
   // --- Constants for project creation
   
   public static final String FILE_MENU = "File"; //$NON-NLS-1$
@@ -35,20 +41,38 @@ public final class WizardConstants {
   
   public static final String NEW_PROJECT_DIALOG_TITLE = "New Project"; //$NON-NLS-1$
   
-  public static final String NEXT_BUTTON = "Next >"; //$NON-NLS-1$
-  
-  public static final String CANCEL_BUTTON = "Cancel"; //$NON-NLS-1$
-  
   public static final String X10_FOLDER = "X10"; //$NON-NLS-1$
   
   public static final String OPEN_ASSOCIATED_PERSPECTIVE_DIALOG_TITLE = "Open Associated Perspective?"; //$NON-NLS-1$
   
-  // --- Constants for project creation with C++ back-end
+  // --- Constants for project import
+  
+  public static final String IMPORT_MENU_ITEM = "Import..."; //$NON-NLS-1$
+  
+  public static final String IMPORT_ARCHIVE_DIALOG_TITLE = "Import"; //$NON-NLS-1$
+  
+  public static final String GENERAL_FOLDER = "General"; //$NON-NLS-1$
+  
+  public static final String IMPORT_ARCHIVE = "Archive File"; //$NON-NLS-1$
+
+  public static final String FROM_ARCHIVE_FILE_FIELD = "From archive file:"; //$NON-NLS-1$
+
+  public static final String INTO_FOLDER_FIELD = "Into folder:"; //$NON-NLS-1$
+
+  public static final String OVERWRITE_EXISTING_CHECKBOX = "Overwrite existing resources without warning"; //$NON-NLS-1$
+
+ // --- Constants for project creation with C++ back-end
   
   public static final String X10_PROJECT_CPP_BACKEND = "X10 Project (C++ back-end)"; //$NON-NLS-1$
   
   public static final String NEW_CPP_PROJECT_NAME = "Project name:"; //$NON-NLS-1$
   
+  public static final String X10_PROJECT_SHELL_CPP_BACKEND = "New X10 project (C++ back-end)"; //$NON-NLS-1$
+  
+  public static final String NEW_CPP_PROJECT_NAME_FIELD = "Project name:"; //$NON-NLS-1$
+  
+  public static final String NEW_X10_PROJECT_HELLO_SOURCE_CHECKBOX= "NEW_X10_PROJECT_HELLO_SOURCE_RADIO"; //$NON-NLS-1$
+
   // --- Constants for project creation with Java back-end
   
   public static final String X10_PROJECT_JAVA_BACKEND = "X10 Project (Java back-end)"; //$NON-NLS-1$
@@ -84,6 +108,16 @@ public final class WizardConstants {
   public static final String NEW_OTHER_SHELL= "New"; //$NON-NLS-1$
 
   public static final String QUICK_OUTLINE_SHELL= ""; // This value is really correct, at least for now //$NON-NLS-1$
+
+  
+  // --- Constants for project and file navigation
+  
+  public static final String PACKAGE_EXPLORER_VIEW_TITLE = "Package Explorer"; //$NON-NLS-1$
+
+  public static final String NAVIGATE_MENU = "Navigate"; //$NON-NLS-1$
+  
+  public static final String OPEN_ITEM = "Open"; //$NON-NLS-1$
+  
 
  
   // --- Constants for project and file deletion
