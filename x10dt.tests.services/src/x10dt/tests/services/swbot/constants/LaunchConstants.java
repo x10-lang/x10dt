c2af05a63b78504e74f488e17dc1b5d338cc9266
Robert Fuhrer <rfuhrer@us.ibm.com>
2011-01-20 22:55:33 +0000

    Added Pattern constants that match "Run As" menu item labels, since the "Run As" menu might vary in contents or ordering. Added constant for "Run As" menu label.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@19610 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.tests.services/src/x10dt/tests/services/swbot/constants/LaunchConstants.java
+++ x10dt.tests.services/src/x10dt/tests/services/swbot/constants/LaunchConstants.java
@@ -7,6 +7,8 @@
  *******************************************************************************/
 package x10dt.tests.services.swbot.constants;
 
+import java.util.regex.Pattern;
+
 /**
  * Contains string constants of the X10DT launch configurations.
  * 
@@ -16,31 +18,40 @@ package x10dt.tests.services.swbot.constants;
 public final class LaunchConstants {
   
   public static final String RUN_MENU = "Run"; //$NON-NLS-1$
-  
+
+  public static final String RUN_AS_MENU = "Run As"; //$NON-NLS-1$
+
   public static final String RUN_HISTORY_ITEM = "Run History"; //$NON-NLS-1$
-  
+
   public static final String RUN_CONFS_MENU_ITEM = "Run Configurations..."; //$NON-NLS-1$
-  
+
   public static final String RUN_CONF_DIALOG_TITLE = "Run Configurations"; //$NON-NLS-1$
-  
+
   public static final String NEW_CONF_CONTEXT_MENU = "New"; //$NON-NLS-1$
-  
+
   public static final String RUN_BUTTON = "Run"; //$NON-NLS-1$
-  
+
   // --- Java back-end specific
-  
+
   public static final String JAVA_BACK_END_PROJECT_DIALOG_NAME_FIELD = "Name:"; //$NON-NLS-1$
 
   // --- C++ back-end specific
-  
+
   public static final String NEW_CPP_LAUNCH_CONFIG = "X10 Application (C++ back-end)"; //$NON-NLS-1$
-  
+
   public static final String JAVA_BACK_END_LAUNCH_CONFIG_TYPE= "X10 Application (Java back-end)"; //$NON-NLS-1$
 
+  // The following two patterns are needed b/c the "Run As" menu has different contents
+  // depending on what installed plugins contribute to that menu. Also, the order, and
+  // therefore the numeric shortcuts, seem unreliable.
+  public static final Pattern RUN_AS_X10_APPLICATION_CPP_BACK_END_PAT= Pattern.compile("(&[1-9] &)?X10 application \\(C\\+\\+ back-end\\).*"); //$NON-NLS-1$
+
+  public static final Pattern RUN_AS_X10_APPLICATION_JAVA_BACK_END_PAT= Pattern.compile("(&[1-9] )?X10 Application \\(Java back-end\\).*");
+
   public static final String CPP_LAUNCH_CONFIG_APPLICATION_TAB = "Application"; //$NON-NLS-1$
-  
+
   public static final String CPP_LAUNCH_CONFIG_X10_PROJECT = "X10 Project"; //$NON-NLS-1$
-  
+
   public static final String CPP_LAUNCH_CONFIG_MAIN_CLASS = "Main class"; //$NON-NLS-1$
 
   public static final String JAVA_BACK_END_LAUNCH_CONFIG_MAIN_TAB= "Main"; //$NON-NLS-1$
@@ -50,7 +61,7 @@ public final class LaunchConstants {
   public static final String JAVA_BACK_END_LAUNCH_CONFIG_MAIN_CLASS= "Main class:"; //$NON-NLS-1$
 
   // --- Private code
-  
+
   private LaunchConstants() {}
 
 }
