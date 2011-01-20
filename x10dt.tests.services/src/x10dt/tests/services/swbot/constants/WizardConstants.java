4171699d4b17df966f9ee9aecb9964529b5af525
Robert Fuhrer <rfuhrer@us.ibm.com>
2011-01-20 22:54:08 +0000

    Fixed value of NEW_X10_PROJECT_HELLO_SOURCE_CHECKBOX, which was totally bogus.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@19609 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.tests.services/src/x10dt/tests/services/swbot/constants/WizardConstants.java
+++ x10dt.tests.services/src/x10dt/tests/services/swbot/constants/WizardConstants.java
@@ -73,7 +73,7 @@ public final class WizardConstants {
   
   public static final String NEW_CPP_PROJECT_NAME_FIELD = "Project name:"; //$NON-NLS-1$
   
-  public static final String NEW_X10_PROJECT_HELLO_SOURCE_CHECKBOX= "NEW_X10_PROJECT_HELLO_SOURCE_RADIO"; //$NON-NLS-1$
+  public static final String NEW_X10_PROJECT_HELLO_SOURCE_CHECKBOX= "Create a sample 'Hello World' X10 application"; //$NON-NLS-1$
 
   // --- Constants for project creation with Java back-end
   
