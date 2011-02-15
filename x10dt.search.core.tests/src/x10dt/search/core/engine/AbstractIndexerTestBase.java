3e36abec68938574ea868370620256f83062388f
Emmanuel Geay <egeay@us.ibm.com>
2011-02-15 18:46:48 +0000

    Improved robustness for the runs by waiting the project creation is ready before returning it for testing.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@20129 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.search.core.tests/src/x10dt/search/core/engine/AbstractIndexerTestBase.java
+++ x10dt.search.core.tests/src/x10dt/search/core/engine/AbstractIndexerTestBase.java
@@ -14,17 +14,52 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
 import org.osgi.framework.Bundle;
 
+import x10dt.core.X10DTCorePlugin;
+import x10dt.core.utils.X10DTCoreConstants;
 import x10dt.tests.services.pde.utils.ProjectUtils;
 
 @SuppressWarnings("nls")
-abstract class AbstractIndexerTestBase {
+abstract class AbstractIndexerTestBase implements IResourceChangeListener {
   
-  // --- Code for descendants
+  // --- Interface methods implementation
+  
+  public final void resourceChanged(final IResourceChangeEvent event) {
+    final IResourceDelta rootResourceDelta = event.getDelta();
+    for (final IResourceDelta resourceDelta : rootResourceDelta.getAffectedChildren()) {
+      if ((resourceDelta.getResource().getType() == IResource.PROJECT) && 
+          (resourceDelta.getKind() == IResourceDelta.CHANGED)) {
+        final IProject project = (IProject) resourceDelta.getResource();
+        try {
+          if (project.hasNature(X10DTCorePlugin.X10_CPP_PRJ_NATURE_ID) || 
+              project.hasNature(X10DTCorePlugin.X10_PRJ_JAVA_NATURE_ID)) {
+            final IJavaProject javaProject = JavaCore.create(project);
+            for (final IClasspathEntry cpEntry : javaProject.getRawClasspath()) {
+              if (X10DTCoreConstants.X10_CONTAINER_ENTRY_ID.equals(cpEntry.getPath().toString())) {
+                this.fIsProjectReady = true;
+                break;
+              }
+            }
+          }
+        } catch (CoreException except) {
+          assertTrue(except.getMessage(), false);
+        }
+      }
+    }
+  }
+  
+  // --- Code for sub-classes
   
   protected final IProject createProjectFromX10Dist(final String projectName, final String data,
                                                     final EProjectBackEnd backEnd) throws CoreException, URISyntaxException, 
@@ -46,6 +81,9 @@ abstract class AbstractIndexerTestBase {
   @SuppressWarnings("null")
   protected final IProject createProject(final String projectName, final String bundleName, final String data,
                                          final EProjectBackEnd backEnd) throws CoreException, URISyntaxException, IOException {
+    this.fIsProjectReady = false;
+    ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
+    
     final Bundle bundle = Platform.getBundle(bundleName);
     assertNotNull(String.format("Unable to find bundle '%s'", bundleName), bundle);
     ResourcesPlugin.getWorkspace().getDescription().setAutoBuilding(false);
@@ -63,7 +101,21 @@ abstract class AbstractIndexerTestBase {
     assertNotNull("X10 Project Back-End not supported", project); 
     assertTrue(project.exists());
     
+    while (! this.fIsProjectReady) {
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException except) {
+        // Simply forgets.
+      }
+    }
+    
+    ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+    
     return project;
   }
+  
+  // --- Fields
+  
+  private boolean fIsProjectReady;
 
 }
