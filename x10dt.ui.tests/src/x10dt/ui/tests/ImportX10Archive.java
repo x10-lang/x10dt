5d0214bbe8f8fa94331329432e74fed75e1bee47
Rick Lesniak <lesniakr@domain>
2011-03-04 18:22:46 +0000

    Superclass of JUnit test class ImportCppArchiveTest.java. Contains the methods used in the JUnit test
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@20480 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui.tests/src/x10dt/ui/tests/ImportX10Archive.java
+++ x10dt.ui.tests/src/x10dt/ui/tests/ImportX10Archive.java
@@ -73,15 +73,16 @@ import x10dt.ui.tests.waits.X10DT_Conditions;
 public class ImportX10Archive extends X10DTTestBase {
 
 /*
- * Support methods for testing x10 projects imported from archives
- */
-  
-  //TODO: pass in constants as arguments, so we can use this method in other test modules
+	 * Support methods for testing x10 projects imported from archives
+	 */
+
+//TODO: pass in constants as arguments, so we can use this method in other test modules
   public void importArchive(BackEndType backEnd, String projectName, String archiveName, String classSrcfileName) throws Exception {
 
 	  Boolean createHello = false;
 	  String archivePath = null;
 	  	  
+System.out.println("Import: find archive file '" + archiveName + "'");
 	  // Locate the archive file in the file system - get the full file path
 	  ClassLoader cl = getClass().getClassLoader();		//archive file must be on the build path
 	  URL archiveURL = cl.getResource(archiveName);	//find the file
@@ -94,6 +95,7 @@ public class ImportX10Archive extends X10DTTestBase {
 	  }
 
 
+	  System.out.println("creating project '" + projectName + "'");
 	  if (backEnd == BackEndType.javaBackEnd) {
 		  // create a java back end project to import the archive into
 		  createJavaBackEndProject(projectName, createHello);
@@ -106,8 +108,15 @@ public class ImportX10Archive extends X10DTTestBase {
 		  topLevelBot.waitUntil(Conditions.waitForEditor(new EditorMatcher("Hello.x10")));
 	  }
 
+System.out.println("Import archive file");
 	  //import the archive
 	  importArchiveToX10Project(archivePath, projectName + "/src", true);
+
+System.out.println("wait for import build to finish");
+
+		waitForBuildToFinish();
+
+System.out.println("import build is finished");
 
 	  //open the imported file in an editor view
 	  openX10FileInEditor(projectName, classSrcfileName);
@@ -164,6 +173,7 @@ public class ImportX10Archive extends X10DTTestBase {
 
 	  boolean matches = true;				//verification result. Assume the best!
 
+System.out.println("verifying X10 program's console output");
 	  //used to synchronize expected text block with actual output
 	  int startAtLine = 0;					//First line number in actual output to begin compare operation
 	  String firstExpectedLine = null;		//Text of first expected output line
@@ -183,6 +193,8 @@ public class ImportX10Archive extends X10DTTestBase {
 	  SWTBotView consoleView = new SWTBotView(waitForConsole.get(0), topLevelBot);
 	  //wait for some output to show up
 	  consoleView.bot().waitUntil(X10DT_Conditions.viewHasText(consoleView), 90000, 500);
+
+	  dumpConsole(topLevelBot);		// echo program's console output to the development console
 
 	  //load the text of the actual output
 	  List<String> actualLines = consoleView.bot().styledText().getLines();
@@ -258,49 +270,37 @@ public class ImportX10Archive extends X10DTTestBase {
    */
   
   //  one or more C++ backend platform configurations are loaded from an xml file. A run is made for each one
-  public PlatformConfig ConfigureAndRunCppProject(String projectName, String className, int numPlaces, String xmlFileName, String configID, List<String> expectedOutput) throws IOException {
+  public Boolean ConfigureAndRunCppProject(String projectName, String className, PlatformConfig platformConfig, List<String> expectedOutput) throws IOException {
 
-	  Document xmlConfigurations = loadXML(xmlFileName);
+	  Boolean result = false;
 
-	  PlatformConfig platformConfig = null;
-
-	  if (! configID.equals(null) ) {	//run a specific configuration
-		  Element configElement = xmlConfigurations.getElementById(configID);
-		  if (configElement != null) {
-			  platformConfig = ExtractConfigFromXML(configElement) ;
-			  setCppPlatformConnectionConfig(projectName, platformConfig);
-			  RunCppProject(projectName, className, platformConfig, expectedOutput);
-		  }
-		  else {
-			  Assert.fail("config id=" + configID + " not found in " + xmlFileName);
-		  }
+	  if (platformConfig != null) {
+		  setCppPlatformConnectionConfig(projectName, platformConfig);
+		  result = RunCppProject(projectName, className, platformConfig, expectedOutput);
 	  }
-	  else {		//run them all	  
-		  NodeList xmlConfigList = xmlConfigurations.getElementsByTagName("config");
 
-		  for (int xmlConfigNum = 0; xmlConfigNum < xmlConfigList.getLength(); xmlConfigNum++) {
-			  platformConfig = ExtractConfigFromXML((Element) xmlConfigList.item(xmlConfigNum));
-			  setCppPlatformConnectionConfig(projectName, platformConfig);
-			  RunCppProject(projectName, className, platformConfig, expectedOutput);
-		  }
-	  }
-	  return platformConfig;
+	  return result;
   }
  
+
   //  one or more C++ backend platform configurations are loaded from an xml file. A run is made for each one
   public Boolean RunCppProject(String projectName, String className, PlatformConfig platformConfig, List<String> expectedOutput) throws IOException {
 
+System.out.println("open Parallel Runtime perspective");
+	  //the program output will be in the Console view of the Parallel Runtime perspective, so let's get into that
+	  openPerspective(topLevelBot, "Parallel Runtime");	  //switch to Parallel Runtime perspective
+
+System.out.println("create and run '" + platformConfig.configName + "'");
 	  //run the program
 	  createAndRunCPPBackEndLaunchConfig(platformConfig.configName, projectName, className, platformConfig.numPlaces);			  
 
+System.out.println("verify console output");
 	  //Well, let's see if it worked
-	  //It's going to pop up the Parallel Runtime perspective, so let's get a handle to that
-	  topLevelBot.perspectiveByLabel("Parallel Runtime").activate();
-
 	  // verify that the actual output matches the expected output
 	  boolean match = verifyConsoleOutput(expectedOutput); //$NON-NLS-1$
 	  Assert.assertTrue("ImportArchiveTest: Console output does not match", match); //$NON-NLS-1$
 
+System.out.println("Go back to the X10 perspective");
 	  //Go back to the X10 perspective
 	  topLevelBot.perspectiveByLabel("X10").activate();
 
@@ -313,23 +313,23 @@ public class ImportX10Archive extends X10DTTestBase {
 
 	  try {
 
-		  platformConfig.configName = getTagString("configName", xmlConfigElement);
-		  platformConfig.description = getTagString("description", xmlConfigElement);
+		  platformConfig.configName = getTagString("configName", xmlConfigElement, 0);
+		  platformConfig.description = getTagString("description", xmlConfigElement, 0);
 
-		  platformConfig.useLocalConnection = getTagBoolean("useLocalConnection", xmlConfigElement);
+		  platformConfig.useLocalConnection = getTagBoolean("useLocalConnection", xmlConfigElement, 0);
 
 		  //target configuration
 		  NodeList xmlTargetNode = xmlConfigElement.getElementsByTagName("target");			  
 		  Element xmlTargetElement = (Element) xmlTargetNode.item(0);
-		  platformConfig.os = OS.get(getTagString("osType", xmlTargetElement));
-		  platformConfig.arch = Architecture.get(getTagString("architectureType", xmlTargetElement));	  
-		  platformConfig.set64bit = getTagBoolean("set64BitSpace", xmlTargetElement);				  
+		  platformConfig.os = OS.get(getTagString("osType", xmlTargetElement, 0));
+		  platformConfig.arch = Architecture.get(getTagString("architectureType", xmlTargetElement, 0));	  
+		  platformConfig.set64bit = getTagBoolean("set64BitSpace", xmlTargetElement, 0);				  
 
 		  //x10 configuration
 		  NodeList xmlX10Node = xmlConfigElement.getElementsByTagName("x10");			  
 		  Element xmlX10Element = (Element) xmlX10Node.item(0);
 		  
-		  platformConfig.numPlaces = getTagInteger("numPlaces", xmlX10Element);
+		  platformConfig.numPlaces = getTagInteger("numPlaces", xmlX10Element, 0);
 		  if (platformConfig.numPlaces == 0)	//num places specified?
 			  platformConfig.numPlaces = 4;		// no - set default;
 
@@ -337,8 +337,8 @@ public class ImportX10Archive extends X10DTTestBase {
 		  //communication interface configuration
 		  NodeList xmlCommNode = xmlConfigElement.getElementsByTagName("commInterface");			  
 		  Element xmlCommElement = (Element) xmlCommNode.item(0);
-		  platformConfig.interfaceType = CommInterface.getType(getTagString("interfaceType", xmlCommElement));    
-		  platformConfig.interfaceMode = CommInterface.getMode(getTagString("interfaceMode", xmlCommElement));
+		  platformConfig.interfaceType = CommInterface.getType(getTagString("interfaceType", xmlCommElement, 0));    
+		  platformConfig.interfaceMode = CommInterface.getMode(getTagString("interfaceMode", xmlCommElement, 0));
 
 
 		  if (platformConfig.useLocalConnection) {			//running on local machine
@@ -351,31 +351,32 @@ public class ImportX10Archive extends X10DTTestBase {
 			  //remote connection configuration
 			  NodeList xmlRemoteNode = xmlConfigElement.getElementsByTagName("remoteConnection");			  
 			  Element xmlRemoteElement = (Element) xmlRemoteNode.item(0);
-			  platformConfig.connectionName 	= getTagString("connectionName", xmlRemoteElement);     
-			  platformConfig.remoteHostName 	= getTagString("remoteHostName", xmlRemoteElement);    
-			  platformConfig.remoteHostPort 	= getTagInteger("remoteHostPort", xmlRemoteElement);    
-			  platformConfig.remoteHostUser 	= getTagString("remoteHostUser", xmlRemoteElement);    
-			  platformConfig.usePassword 		= getTagBoolean("usePassword", xmlRemoteElement);      
-			  platformConfig.remoteHostPassword = getTagString("remoteHostPassword", xmlRemoteElement);
+			  platformConfig.connectionName 	= getTagString("connectionName", xmlRemoteElement, 0);     
+			  platformConfig.remoteHostName 	= getTagString("remoteHostName", xmlRemoteElement, 0);    
+			  platformConfig.remoteHostPort 	= getTagInteger("remoteHostPort", xmlRemoteElement, 0);    
+			  platformConfig.remoteHostUser 	= getTagString("remoteHostUser", xmlRemoteElement, 0);    
+			  platformConfig.usePassword 		= getTagBoolean("usePassword", xmlRemoteElement, 0);      
+			  platformConfig.remoteHostPassword = getTagString("remoteHostPassword", xmlRemoteElement, 0);
+			  platformConfig.remoteKeyFile		= getTagString("remoteKeyFile", xmlRemoteElement, 0);
 
 			  //Port forwarding settings
 			  NodeList xmlPortNode = xmlConfigElement.getElementsByTagName("portForwarding");			  
 			  Element xmlPortElement = (Element) xmlPortNode.item(0);
-			  platformConfig.usePortForwarding        	= getTagBoolean("usePortForwarding", xmlPortElement);    
-			  platformConfig.portForwardingTimeout    	= getTagString("portForwardingTimeout", xmlPortElement);    
-			  platformConfig.portForwardingLocalAddress	= getTagString("portForwardingLocalAddress", xmlPortElement);      
+			  platformConfig.usePortForwarding        	= getTagBoolean("usePortForwarding", xmlPortElement, 0);    
+			  platformConfig.portForwardingTimeout    	= getTagString("portForwardingTimeout", xmlPortElement, 0);    
+			  platformConfig.portForwardingLocalAddress	= getTagString("portForwardingLocalAddress", xmlPortElement, 0);      
 
 			  //Remote compilation settings
 			  NodeList xmlCompileNode = xmlConfigElement.getElementsByTagName("remotePlatform");
 			  Element xmlCompileElement = (Element) xmlCompileNode.item(0);
 			  if (xmlCompileElement != null)
 			  {
-				  platformConfig.outputFolder       = getTagString("outputFolder", xmlCompileElement);
-				  platformConfig.useSelectedPGAS    = getTagBoolean("useSelectedPGAS", xmlCompileElement);      
-				  platformConfig.remoteDistribution = getTagString("remoteDistribution", xmlCompileElement);    
-				  platformConfig.remotePGASDist     = getTagString("remotePGASDist", xmlCompileElement);    
-				  platformConfig.debuggerFolder 	= getTagString("debuggerFolder", xmlCompileElement);    
-				  platformConfig.debuggingPort     	= getTagInteger("debuggingPort", xmlCompileElement); 
+				  platformConfig.outputFolder       = getTagString("outputFolder", xmlCompileElement, 0);
+				  platformConfig.useSelectedPGAS    = getTagBoolean("useSelectedPGAS", xmlCompileElement, 0);      
+				  platformConfig.remoteDistribution = getTagString("remoteDistribution", xmlCompileElement, 0);    
+				  platformConfig.remotePGASDist     = getTagString("remotePGASDist", xmlCompileElement, 0);    
+				  platformConfig.debuggerFolder 	= getTagString("debuggerFolder", xmlCompileElement, 0);    
+				  platformConfig.debuggingPort     	= getTagInteger("debuggingPort", xmlCompileElement, 0); 
 			  }
 		  } //if using remote connection
 
