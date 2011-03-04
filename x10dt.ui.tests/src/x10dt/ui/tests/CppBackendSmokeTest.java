b96740d93ef531e7a6ba2e87a9fbc50ef5f291b6
Rick Lesniak <lesniakr@domain>
2011-03-04 18:26:00 +0000

    Defines the JUnit test suite which comprises the Smoke Test. The suite includes ImportCppArchiveTest and CppBackEndTestConfigs
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@20481 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui.tests/src/x10dt/ui/tests/CppBackendSmokeTest.java
+++ x10dt.ui.tests/src/x10dt/ui/tests/CppBackendSmokeTest.java
@@ -26,7 +26,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.junit.runners.Suite;
 
 import x10dt.core.utils.Timeout;
 import x10dt.tests.services.swbot.utils.SWTBotUtils;
@@ -34,42 +34,13 @@ import x10dt.tests.services.swbot.utils.SWTBotUtils;
 /**
  * @author lesniakr@us.ibm.com
  */
-@RunWith(SWTBotJunit4ClassRunner.class)
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+						ImportCppArchiveTest.class,
+						CppBackendTestConfigs.class
+        			})
+        
 public class CppBackendSmokeTest extends ImportX10Archive {
-
-	private static final String CLASS_NAME = "QSort";  //$NON-NLS-1$					//imported class name
-
-	private static final String CLASS_SRCFILE_NAME = CLASS_NAME + ".x10"; //$NON-NLS-1$	//imported source file name
-
-	private static final String ARCHIVE_NAME = "ArchiveTestFile.zip"; //$NON-NLS-1$		// archive file name
-	
-	private static final String PROJECT_NAME_CPPBACK = "ArchiveTest_CPPBack"; //$NON-NLS-1$	//new empty C++ backend project to accept the import
-
-	private static final int DEFAULT_NUM_PLACES = 4;	//default
-
-	//we expect to see this console output when we run the imported archive
-	public static final List<String> EXPECTED_OUTPUT = Arrays.asList(	"size of array: 1000",
-																		"array is now sorted", 
-																		"++++++ Test succeeded spectacularly!"
-																	);
-	
-	//static initialization of TypeSearchInfo 
-	//		/*search*/					-	The search string
-	//		/*type*/					- 	The type we're looking for
-	//		/*# to find*/				-	The minimum number of search results we're looking for
-	//		/*file*/					-	The source file where we're expecting to find the type
-	//		/*expected declaration*/	-	The expected type declaration text in the source file
-	public static final List<TypeSearchInfo> declarationCheckList = Arrays.asList
-	(
-							/*search*/	/*type*/	 /*# to find*/	/*file*/			/*expected declaration*/
-		new TypeSearchInfo("QS*",		"QSortable",	2,			"QSort.x10",		"public class QSortable(theArray: SortableArray)"								),
-		new TypeSearchInfo("Pos*",		"Position",		1,			"TriangleTest.x10",	"class Position(x: Int, y: Int)"												),
-		new TypeSearchInfo("Ar*",		"Array",		5,			"Array.x10",		"public final class Array[T] ("													),
-		new TypeSearchInfo("Ar*",		"ArrayList",	5,			"ArrayList.x10",	"public class ArrayList[T] extends AbstractCollection[T] implements List[T] {"	),
-		new TypeSearchInfo("QSort*",	"QSort",		2,			"QSort.x10",		"public class QSort"															)
-	);
-
-//	static Document xmlConfigurations;		//loaded from XML - this document contains specifics of one or more x10 platform configurations
 
 	@BeforeClass
 	public static void beforeClass() throws Exception {
@@ -77,54 +48,13 @@ public class CppBackendSmokeTest extends ImportX10Archive {
 		SWTBotUtils.closeWelcomeViewIfNeeded(topLevelBot);
 		topLevelBot.perspectiveByLabel("X10").activate();
 		SWTBotPreferences.TIMEOUT = Timeout.SIXTY_SECONDS;
-		
-//		xmlConfigurations = loadXML("CppPlatformConfigs.xml");
 	}
 	
-	@After
-	public void after() throws Exception {
+	@AfterClass
+	public static void afterClass() throws Exception {
+		SWTBotUtils.saveAllDirtyEditors(topLevelBot);
 		SWTBotUtils.closeAllEditors(topLevelBot);
 		SWTBotUtils.closeAllShells(topLevelBot);
 	}
 
-	@AfterClass
-	public static void afterClass() throws Exception {
-		SWTBotUtils.saveAllDirtyEditors(topLevelBot);
-	}
-
-	//
-	@Test
-	public void test_ImportCPPArchive() throws Exception {
-		importArchive(BackEndType.cppBackEnd, PROJECT_NAME_CPPBACK, ARCHIVE_NAME, CLASS_SRCFILE_NAME);
-	}
-
-	//
-	@Test
-	public void test_OpenType() throws Exception {
-		validateOpenType(declarationCheckList);
-	}
-
-	//
-	@Test
-	public void test_SearchType() throws Exception {
-		validateTypeSearch(declarationCheckList);
-	}
-
-	//
-	@Test
-	//use local platform config w/ sockets runtime
-	public void test_LocalRun() throws Exception {
-		 ConfigureAndRunCppProject(PROJECT_NAME_CPPBACK, CLASS_NAME, 
-				 DEFAULT_NUM_PLACES, "CppPlatformConfigs.xml", 
-				 "localSocketsConfig", EXPECTED_OUTPUT); 
-	}
-
-//	//
-//	@Test
-//	//use remote Linux platform config w/ sockets runtime
-//	public void test_RemoteLinux() throws Exception {
-//		 ConfigureAndRunCppProject(PROJECT_NAME_CPPBACK, CLASS_NAME, 
-//				 DEFAULT_NUM_PLACES, "CppPlatformConfigs.xml", 
-//				 "remoteLinuxSocketsConfig", EXPECTED_OUTPUT); 
-//	}
 }
