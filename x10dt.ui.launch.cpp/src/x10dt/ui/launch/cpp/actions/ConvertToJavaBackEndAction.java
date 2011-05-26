d627f047d2646a1486267c68dc547ef10a014bf6
Mandana Vaziri <mvaziri@us.ibm.com>
2011-05-26 21:23:54 +0000

    Change to close the platform conf editor when converting from C++ backend to Java backend.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@21974 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui.launch.cpp/src/x10dt/ui/launch/cpp/actions/ConvertToJavaBackEndAction.java
+++ x10dt.ui.launch.cpp/src/x10dt/ui/launch/cpp/actions/ConvertToJavaBackEndAction.java
@@ -15,7 +15,11 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.imp.editor.EditorUtility;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 
 import x10dt.ui.launch.core.actions.AbstractConvertX10ProjectAction;
 import x10dt.ui.launch.core.actions.IPlatformConfHider;
@@ -37,8 +41,17 @@ public final class ConvertToJavaBackEndAction extends AbstractConvertX10ProjectA
   protected IPlatformConfHider createPlatformConfHider(final IProject project){
 	  return new IPlatformConfHider(){
 		public void hide() {
+			
 			final IFile platformConfFile = X10PlatformConfFactory.getFile(project);
 			
+			// --- Close the editor if it's open.
+			final IWorkbenchPage activePage= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+			IEditorPart editor= EditorUtility.isOpenInEditor(platformConfFile.getFullPath());
+			if (editor != null) {
+				activePage.closeEditor(editor, false);
+			}
+			
+			// --- Rename the platformConf file to start with "."
 			IFile hiddenFile = ResourcesPlugin.getWorkspace().getRoot().getFile(platformConfFile.getFullPath().removeLastSegments(1).append(new Path("." + platformConfFile.getName())));
 			IFileStore hiddenStore = EFS.getLocalFileSystem().getStore(hiddenFile.getLocationURI());
 			try {
@@ -46,6 +59,8 @@ public final class ConvertToJavaBackEndAction extends AbstractConvertX10ProjectA
 			} catch (CoreException e) {
 				//TODO
 			}
+			
+			
 			 
 		}
 	  };
