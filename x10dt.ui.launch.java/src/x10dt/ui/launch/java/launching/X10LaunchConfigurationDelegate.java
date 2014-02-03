3a35807843a78f079b019fa5800d5c5237c2bde2
Mandana Vaziri <mvaziri@us.ibm.com>
2014-02-03 15:56:34 +0000

    X10DT -- Bug fix for Windows local launch.
    git-svn-id: svn://svn.code.sf.net/p/x10/code/x10dt/trunk@27332 a326200e-df2c-42e0-81fa-597e909af41d

--- x10dt.ui.launch.java/src/x10dt/ui/launch/java/launching/X10LaunchConfigurationDelegate.java
+++ x10dt.ui.launch.java/src/x10dt/ui/launch/java/launching/X10LaunchConfigurationDelegate.java
@@ -350,8 +350,8 @@ public final class X10LaunchConfigurationDelegate implements ILaunchConfiguratio
     if (isRemote) {
       sb.append(String.format("%s -cp \"%s\" %s", getVMArguments(configuration, true), jarPath.toString(), mainTypeName)); //$NON-NLS-1$
     } if (ConfUtils.getLocalOS() == ETargetOS.WINDOWS) { //Local and Windows
-    	sb.append(String.format("java %s -cp \"%s\" %s", getVMArguments(configuration, false), buildClassPath(configuration), //$NON-NLS-1$ 
-                "x10.x10rt.Launcher", mainTypeName + "$$Main")); //$NON-NLS-1$
+    	sb.append(String.format("java %s -cp \"%s\" %s %s", getVMArguments(configuration, false), buildClassPath(configuration), //$NON-NLS-1$ 
+                "x10.x10rt.Launcher", mainTypeName)); //$NON-NLS-1$
   	} else {
       sb.append(String.format("java %s -cp \"%s\" %s", getVMArguments(configuration, false), buildClassPath(configuration), //$NON-NLS-1$ 
                               mainTypeName + "$$Main")); //$NON-NLS-1$
